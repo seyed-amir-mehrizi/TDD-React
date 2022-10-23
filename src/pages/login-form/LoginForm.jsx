ca49392ecbe430f4229a2be1feeee9895443cdb5
seyed-amir-mehrizi <60378911+seyed-amir-mehrizi@users.noreply.github.com>
2022-10-23 14:04:32 +0330

    finish test about inputs

--- src/pages/login-form/LoginForm.jsx
+++ src/pages/login-form/LoginForm.jsx
@@ -15,6 +15,28 @@ function LoginForm() {
             className="form-control"
           />
         </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="form-control"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="conform-password" className="form-label">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            id="conform-password"
+            name="conform-password"
+            className="form-control"
+          />
+        </div>
       </form>
     </div>
   );
--- src/pages/login-form/__test__/LoginForm.test.js
+++ src/pages/login-form/__test__/LoginForm.test.js
@@ -5,7 +5,11 @@ describe("Login Form Component", () => {
   test("inputs should be initially empty", () => {
     render(<LoginForm />);
     const emailInputElement = screen.getByRole("textbox");
+    const passwordInputElement = screen.getByLabelText("Password");
+    const confirmedPasswordInputElement = screen.getByLabelText("Confirm Password");
     expect(emailInputElement.value).toBe('');
+    expect(passwordInputElement.value).toBe('');
+    expect(confirmedPasswordInputElement.value).toBe('');
 
   });
 });
